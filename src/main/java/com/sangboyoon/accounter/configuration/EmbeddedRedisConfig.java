2b70314dbdc9805d61b10b5d8d0847e8a5f4d192
ksone02 <eksovus@gmail.com>
2022-09-29 09:35:11 +0900

    [Chore] Embedded Redis Remove

--- src/main/java/com/sangboyoon/accounter/configuration/EmbeddedRedisConfig.java
+++ src/main/java/com/sangboyoon/accounter/configuration/EmbeddedRedisConfig.java
@@ -1,36 +1,36 @@
-package com.sangboyoon.accounter.configuration;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-
-@Configuration
-@Slf4j
-public class EmbeddedRedisConfig {
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    private RedisServer redisServer;
-
-    @PostConstruct
-    public void redisServer() throws IOException {
-        log.info("Connect to Embedded-Redis");
-        redisServer = RedisServer.builder()
-                .port(redisPort)
-                .setting("maxmemory 128M")
-                .build();
-        redisServer.start();
-    }
-
-    @PreDestroy
-    public void stopRedis() {
-        if (redisServer != null && redisServer.isActive()) {
-            redisServer.stop();
-        }
-    }
-}
+//package com.sangboyoon.accounter.configuration;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import redis.embedded.RedisServer;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.PreDestroy;
+//import java.io.IOException;
+//
+//@Configuration
+//@Slf4j
+//public class EmbeddedRedisConfig {
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    private RedisServer redisServer;
+//
+//    @PostConstruct
+//    public void redisServer() throws IOException {
+//        log.info("Connect to Embedded-Redis");
+//        redisServer = RedisServer.builder()
+//                .port(redisPort)
+//                .setting("maxmemory 128M")
+//                .build();
+//        redisServer.start();
+//    }
+//
+//    @PreDestroy
+//    public void stopRedis() {
+//        if (redisServer != null && redisServer.isActive()) {
+//            redisServer.stop();
+//        }
+//    }
+//}
